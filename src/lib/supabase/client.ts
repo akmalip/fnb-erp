29c489ade25bed69ae354eb28d154277b3f6af0a
akmalip <134563320+akmalip@users.noreply.github.com>
2026-04-18 23:36:05 +0700

    fix: keep hardcoded key with downgraded ssr 0.4.0

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -6,3 +6,4 @@ export function createClient() {
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqdnhzZG5icGlkamR1dnBwcW51Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0NDQwOTcsImV4cCI6MjA5MjAyMDA5N30.xYElZNXwo2u9JMPJZ07YYeuOxkS9y76Yf2pZkLyFO44'
   )
 }
+
