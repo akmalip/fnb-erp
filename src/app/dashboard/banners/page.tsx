e0072ddd0c82d3869619f230e7cbb647941fea6e
akmalip <134563320+akmalip@users.noreply.github.com>
2026-04-19 12:21:10 +0700

    fix: force-dynamic src/app/dashboard/banners/page.tsx

--- src/app/dashboard/banners/page.tsx
+++ src/app/dashboard/banners/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import { useEffect, useState, useRef } from 'react'
 import { getAllBannersByOutlet, upsertBanner, deleteBanner } from '@/lib/supabase/queries'
 import { createClient } from '@/lib/supabase/client'
